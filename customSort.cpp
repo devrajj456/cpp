848e03877d4bbae568e1f3e8be8fe6ffe5c6b112
Devraj Mandal <ece.22beci77@silicon.ac.in>
2025-09-14 11:06:45 +0000

    Initial commit

--- customSort.cpp
+++ customSort.cpp
@@ -1,17 +1,34 @@
+// #include<iostream>
+// #include<algorithm>
+// #include<vector>
+// using namespace std;
+// bool compare(int a,int b){
+//     return a>b;
+// }
+// int main(){
+//     vector<int>v={7, 5, 2, 1, 4, 3};
+//     sort(v.begin(),v.end(),compare);
+
+//     cout<<"Sorted vector: "<<endl;
+//     for(auto num:v){
+//         cout<<num<<" ";
+//     }
+//     cout<<endl;
+// }
+
 #include<iostream>
 #include<algorithm>
 #include<vector>
 using namespace std;
-bool compare(int a,int b){
+bool customSort(int a,int b){
     return a>b;
 }
 int main(){
-    vector<int>v={7, 5, 2, 1, 4, 3};
-    sort(v.begin(),v.end(),compare);
+    vector<int>v={1,5,8,9,2,6,9,6,2};
+    sort(v.begin(),v.end(),customSort);
 
-    cout<<"Sorted vector: "<<endl;
-    for(auto num:v){
-        cout<<num<<" ";
+    for(int i=0;i<v.size();i++){
+        cout<<v[i]<<" ";
     }
     cout<<endl;
 }
